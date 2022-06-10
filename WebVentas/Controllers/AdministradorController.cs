8321c2a8d59d03caa1b7b61cf5639873253ea2ce
loren <stinpc@hotmail.com>
2022-06-10 15:48:49 -0500

    vista producto administrador

--- WebVentas/Controllers/AdministradorController.cs
+++ WebVentas/Controllers/AdministradorController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebVentas.Models.Interfaces;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebVentas.Models.ModelBD;
+
+using WebVentas.Models.ModelVista.response;
 
 namespace WebVentas.Controllers
 {
     public class AdministradorController : Controller
     {
+        private IProductoService _sPro;
+        public AdministradorController(IProductoService sPro)
+        {
+
+            this._sPro = sPro;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+
     }
 }
--- WebVentas/Controllers/ProductoController.cs
+++ WebVentas/Controllers/ProductoController.cs
@@ -13,6 +13,7 @@ namespace WebVentas.Controllers
         {
             _Ps = _producto;
         }
+
         public IActionResult Index()
         {
             return View();
@@ -96,6 +97,143 @@ namespace WebVentas.Controllers
             catch(Exception ex)
             {
                 rpta.Success = false; rpta.Message = ex.Message; rpta.Data = null;
+            }
+            return Ok(rpta);
+        }
+
+
+
+        // ProductoAdmin
+        public IActionResult ProductoAdmin()
+        {
+            return View();
+        }
+        public IActionResult MantenimientoProductos(int id = 0)
+        {
+            Producto entity = null;
+            //ViewBag.Categoria = _sCat.GetAll();
+            if (id != 0) entity = _Ps.GetById(id);
+            return PartialView("_MantenimientoProducto", entity ?? new Producto());
+        }
+
+
+        // Services Rest
+        /*
+        [HttpPost]
+        public IActionResult CreateProducto([FromBody] Producto entity)
+        {
+            ResponseData rpta = new ResponseData();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (entity.PkProducto != 0)
+                    {
+
+                    }
+                    else
+                    {
+                        // Falta en IProductoService Validar
+                        var ValidateCodigo = _sPro.ValidarCodigoProducto(entity.Codigo);
+                        var ValidateNombre = _sPro.ValidarNombreProducto(entity.Nombre);
+                        if(ValidateCodigo || ValidateNombre)
+                        {
+                            throw new Exception("Error. Los datos ya se encuentran registrados");
+                        }
+                        
+                        rpta.Data = _sPro.Create(entity);
+                        rpta.Message = "Success";
+                        rpta.State = 200;
+                    }
+                }
+                else { return BadRequest(); }
+            }
+            catch (Exception ex)
+            {
+                rpta.State = 404;
+                rpta.Message = ex.Message;
+                rpta.Data = null;
+            }
+            return Ok(rpta);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProducto([FromBody] Producto entity)
+        {
+            Response rpta = new Response();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (entity.PkProducto != 0)
+                    {
+                        rpta.Data = _sPro.Update(entity);
+                        rpta.Message = "Success.";
+                        rpta.State = 200;
+                    }
+                    else
+                    {
+                        return BadRequest();
+                    }
+
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                rpta.State = 404;
+                rpta.Message = ex.Message;
+                rpta.Data = null;
+            }
+            return Ok(rpta);
+        }
+
+
+        [HttpPost]
+        public IActionResult DesactiveProducto([FromBody] int id)
+        {
+            Response rpta = new Response();
+            try
+            {
+                var DeleteProduct = _sPro.Delete(id);
+                if (DeleteProduct)
+                {
+                    rpta.Data = true;
+                    rpta.Message = "Producto Desactivado";
+                    rpta.State = 200;
+                }
+                else { throw new Exception("Error. El producto no se pudo desactivar"); }
+
+            }
+            catch (Exception ex)
+            {
+                rpta.State = 404;
+                rpta.Data = null;
+                rpta.Message = ex.Message;
+
+            }
+            return Ok(rpta);
+        }*/
+
+        [HttpGet]
+        public IActionResult GetAllProductoP()
+        {
+            ResponseData rpta = new ResponseData();
+            try
+            {
+                rpta.Data = _Ps.GetAllProductos();
+                rpta.State = 200;
+                rpta.Message = "Success.";
+            }
+            catch (Exception ex)
+            {
+                rpta.Data = null;
+                rpta.State = 400;
+                rpta.Message = "Error";
+
             }
             return Ok(rpta);
         }
